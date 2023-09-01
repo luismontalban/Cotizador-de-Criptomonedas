8f0fc14b342b373c07ba5b559c2b297dd86e1512
Luis M <luis.montalban03@gmail.com>
2023-09-01 15:47:47 -0400

    Proyecto OK

---
+++ src/components/Forms.jsx
@@ -0,0 +1,108 @@
+import { useState,useEffect } from 'react'
+import styled from '@emotion/styled'
+import { Error } from './Error'
+
+import useSelectMonedas from '../hooks/useSelectMonedas'
+import { monedas } from '../data/monedas'
+
+
+
+
+const InputSubmit = styled.input`
+    background-color: #9497FF;
+    border: none;
+    width: 100%;
+    padding: 10px;
+    color: #FFF;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 20px;
+    border-radius: 5px;
+    margin-top: 30px;
+    transition: background-color .3s ease;
+
+    &:hover{
+      background-color: #7A7DFE;
+      cursor: pointer;
+    }
+
+`
+
+export const Forms = ({setMonedas}) => {
+
+
+  const [criptos, setCriptos] = useState([])
+  const [error, setError] = useState(false)
+
+  const [moneda, SelectMonedas] = useSelectMonedas('Elige tu Moneda', monedas)
+  const [criptomonedas, SelectCriptomonedas] = useSelectMonedas('Elige tu Criptomoneda', criptos)
+
+  useEffect(() => {
+
+    const consultarApi = async () =>{
+      const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD"
+
+      const respuesta = await fetch(url)
+      const resultado = await respuesta.json()
+
+      const arrayCriptos = resultado.Data.map(cripto =>{
+       
+        const objeto = {
+          id: cripto.CoinInfo.Name,
+          nombre: cripto.CoinInfo.FullName
+        }
+
+
+        return objeto
+
+
+      })
+
+      setCriptos(arrayCriptos)
+
+    }
+
+    consultarApi()
+    
+  }, [])
+  
+
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    
+    if ([moneda, criptomonedas].includes('')) {
+
+      setError(true)
+
+      return
+      
+    }
+
+    setError(false)
+    setMonedas({
+      moneda,
+      criptomonedas
+    })
+
+
+  }
+
+  return (
+    <>
+
+    {error && <Error>Todos los campos son obligatorios</Error>}
+    <form onSubmit={handleSubmit}>
+        <SelectMonedas />
+
+        <SelectCriptomonedas />
+
+        
+
+        <InputSubmit type="submit" value="Cotizar" />
+    </form>
+
+    </>
+  )
+}
